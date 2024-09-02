59c68e0d62c2077c13b830543e82b6a0ced4c9b3
daniabib <danielsabib@gmail.com>
2024-09-02 12:54:47 -0300

    Update handler

--- src/start.sh
+++ src/start.sh
@@ -4,7 +4,7 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-echo "!!! HELLO FROM VERSION: 0.2.3  !!!"
+echo "!!! HELLO FROM VERSION: 0.2.4  !!!"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
